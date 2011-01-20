d5f4128b09fda799a1f1f6cf9cf14f5947665e4d
Dan York <dyork@Lodestar2.com>
2011-01-20 14:45:08 -0500

    Modified httpsample.js to run on port 80

--- httpsample.js
+++ httpsample.js
@@ -6,6 +6,6 @@ var server = http.createServer(function (request, response) {
    console.log("Got a connection");
 });
 
-server.listen(8000);
+server.listen(80);
 
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running on localhost on port 80");
